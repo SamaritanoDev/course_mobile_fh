255fde937c6913e9e005a0adabaab600ae0fe4e4
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2023-07-28 11:55:41 -0500

    add main

--- app_widgets/lib/main.dart
+++ app_widgets/lib/main.dart
@@ -1,20 +1,20 @@
+import 'package:app_widgets/config/router/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello World',
+      routerConfig: appRouter,
     );
   }
 }
